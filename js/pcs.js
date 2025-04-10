12112cd7a6570804b3e79c6e1c3efd722a25f9ce
apidevcl <marcos.much@gmail.com>
2025-04-10 16:38:10 -0400

    creación de piezas básicas del juego

---
+++ js/pcs.js
@@ -0,0 +1,45 @@
+const pcs = [
+    {
+        I: {
+            enabled: 1,
+            color: '#f75a5a',
+            insetPcs1: '0px auto auto 0px',
+            insetPcs2: '0px auto auto 25px',
+            insetPcs3: '0px auto auto 50px',
+            insetPcs4: '0px auto auto 75px'            
+        },
+        O: {
+            enabled: 1,
+            color: '#ffd63a',
+            insetPcs1: '0px auto auto 0px',
+            insetPcs2: '0px auto auto 25px',
+            insetPcs3: '25px auto auto 0px',
+            insetPcs4: '25px auto auto 25px'
+        },
+        L: {
+            enabled: 1,
+            color: '#ffa955',
+            insetPcs1: '25px auto auto 0px',
+            insetPcs2: '50px auto auto 0px',
+            insetPcs3: '75px auto auto 0px',
+            insetPcs4: '75px auto auto 25px'
+        },
+        Z: {
+            enabled: 1,
+            color: '#16c47f',
+            insetPcs1: '50px auto auto 0px',
+            insetPcs2: '50px auto auto 25px',
+            insetPcs3: '75px auto auto 25px',
+            insetPcs4: '75px auto auto 50px'
+        },
+        T: {
+            enabled: 1,
+            color: '#8f87f1',
+            insetPcs1: '50px auto auto 0px',
+            insetPcs2: '50px auto auto 25px',
+            insetPcs3: '50px auto auto 50px',
+            insetPcs4: '75px auto auto 25px'
+        }
+
+    }
+]
