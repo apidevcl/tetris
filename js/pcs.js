5bce455b7832333b7343a69da7fa87d3d6b580d3
apidevcl <marcos.much@gmail.com>
2025-04-11 12:26:58 -0400

    mejoras en el desplazamiento de las piezas por el tablero

--- js/pcs.js
+++ js/pcs.js
@@ -9,7 +9,7 @@ const pcs = [
             insetPcs4: '0px auto auto 75px'            
         },
         O: {
-            enabled: 0,
+            enabled: 1,
             color: '#ffd63a',
             insetPcs1: '0px auto auto 0px',
             insetPcs2: '0px auto auto 25px',
@@ -17,7 +17,7 @@ const pcs = [
             insetPcs4: '25px auto auto 25px'
         },
         L: {
-            enabled: 0,
+            enabled: 1,
             color: '#ffa955',
             insetPcs1: '25px auto auto 0px',
             insetPcs2: '50px auto auto 0px',
@@ -25,7 +25,7 @@ const pcs = [
             insetPcs4: '75px auto auto 25px'
         },
         Z: {
-            enabled: 1,
+            enabled: 0,
             color: '#16c47f',
             insetPcs1: '50px auto auto 0px',
             insetPcs2: '50px auto auto 25px',
@@ -33,7 +33,7 @@ const pcs = [
             insetPcs4: '75px auto auto 50px'
         },
         T: {
-            enabled: 1,
+            enabled: 0,
             color: '#8f87f1',
             insetPcs1: '50px auto auto 0px',
             insetPcs2: '50px auto auto 25px',
